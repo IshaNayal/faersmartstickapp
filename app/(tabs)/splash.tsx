db5d6a6d042562cdea4c5570c7035ca87bc16645
Isha Nayal <ishanayal16@gmail.com>
2025-09-10 14:52:55 +0530

    Create splash.tsx

---
+++ app/(tabs)/splash.tsx
@@ -0,0 +1,45 @@
+import { useRouter } from "expo-router";
+import React, { useEffect } from "react";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.replace("/WelcomeScreen"); // Navigate to Welcome after 2s
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, [router]);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.logoContainer}>
+        <Text style={styles.logoText}>LOGO</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#123255ff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logoContainer: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logoText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#0d1b2a",
+  },
+});
