f27b680f685014b8c662f74517d220801f58a47d
Isha Nayal <ishanayal16@gmail.com>
2025-09-10 14:57:46 +0530

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCBe7ByKvM4HGvRk8f6OoWhtJSadLO6Zro",
+  authDomain: "faersmartstickapp-35ad8.firebaseapp.com",
+  projectId: "faersmartstickapp-35ad8",
+  storageBucket: "faersmartstickapp-35ad8.firebasestorage.app",
+  messagingSenderId: "132163559183",
+  appId: "1:132163559183:web:2cc4c64294359c5b36a87a",
+  measurementId: "G-YXWHY0PZKS"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app); // declare only once
+
+export { auth, db };
