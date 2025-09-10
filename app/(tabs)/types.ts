114dd5bfa834059623b7e11960bd3329b06fecac
Isha Nayal <ishanayal16@gmail.com>
2025-09-10 14:54:00 +0530

    Create types.ts

---
+++ app/(tabs)/types.ts
@@ -0,0 +1,23 @@
+export type RootStackParamList = {  
+  Splash: undefined;
+  Welcome: undefined;
+  SignIn: undefined;
+  SignUp: undefined; 
+  NavigationScreen: undefined; 
+  HomeScreen1: undefined; 
+  AlertScreen: undefined; 
+  SettingsScreen: undefined; 
+  LocationScreen: undefined; 
+  LinkScreen: undefined; 
+  Sidebar: undefined; 
+  ChangePasswordScreen: undefined; 
+  EditProfileScreen: undefined; 
+  NavigationPreferencesScreen: undefined; 
+  About: undefined; 
+
+
+  WelcomeScreen: undefined;
+  SignUpScreen: undefined;
+  LoginScreen: undefined;
+
+};
