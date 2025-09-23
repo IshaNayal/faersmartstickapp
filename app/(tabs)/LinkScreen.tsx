eb33cea71b0372aaedca353194edc30b9d1b5f19
Isha Nayal <ishanayal16@gmail.com>
2025-09-23 09:57:18 +0530

    Update LinkScreen.tsx

--- app/(tabs)/LinkScreen.tsx
+++ app/(tabs)/LinkScreen.tsx
@@ -1,31 +1,60 @@
-import React from "react";
-import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
+import { Image, StyleSheet, Text, View, Switch } from "react-native";
 
 export default function LinkScreen() {
+
+  const [isDeviceOn, setIsDeviceOn] = useState(false);
+
   return (
     <View style={styles.container}>
-      { }
-      <View style={styles.topSection}>
-       
-        <Text style={styles.title}>Connection</Text>
+  
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Connect Smart Stick</Text>
+        <Text style={styles.sectionDesc}>
+          Pair your smart stick to access features like live location, SOS, and activity updates.
+        </Text>
       </View>
 
-      {}
-      <View style={styles.content}>
-        <Image
-          style={styles.icon}
-          source={{ uri: "https://img.icons8.com/ios-filled/50/14b8c4/sos.png" }}
-        />
-        <Text style={styles.description}>Connect Smart Stick</Text>
-        <Text style={styles.status}>Status: Connected</Text>
-        <Text style={styles.deviceInfo}>Device: Stick-ABC123</Text>
 
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Pair Device</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Check Connection</Text>
-        </TouchableOpacity>
+      <View style={styles.deviceStatusRow}>
+        <Text style={styles.deviceStatusText}>Device Status</Text>
+        <View style={styles.switchWrapper}>
+          <Switch
+            value={isDeviceOn}
+            onValueChange={setIsDeviceOn} 
+            thumbColor="#fff"
+            trackColor={{ false: "#102147", true: "#14b8c4" }}
+          />
+        </View>
+      </View>
+      <View style={styles.divider} />
+
+    
+      <Text style={styles.pairedTitle}>Paired Devices</Text>
+
+     
+      <View style={styles.deviceBox}>
+        <View style={styles.deviceRow}>
+          <Image
+            style={styles.stickIcon}
+            source={{ uri: "https://img.icons8.com/color/48/match-stick.png" }}
+          />
+          <Text style={styles.deviceNameConnected}>Stick123</Text>
+          <Text style={styles.deviceStatusConnected}>✓ connected</Text>
+        </View>
+        <Text style={styles.deviceLastSync}>Last Synced : just Now</Text>
+      </View>
+
+ 
+      <View style={styles.deviceBox}>
+        <View style={styles.deviceRow}>
+          <Image
+            style={styles.stickIcon}
+            source={{ uri: "https://img.icons8.com/color/48/match-stick.png" }}
+          />
+          <Text style={styles.deviceNameNotConnected}>Stick130</Text>
+          <Text style={styles.deviceStatusNotConnected}>Not connected</Text>
+        </View>
       </View>
     </View>
   );
@@ -35,65 +64,96 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "rgba(1, 21, 71, 1)",
-    paddingHorizontal: 1, 
+    paddingHorizontal: 20,
+    paddingTop: 35,
   },
-  topSection: {
-    alignItems: "center",
-    marginTop: 80, 
-    marginBottom: 40,
+  section: {
+    marginBottom: 7,
+    justifyContent: "center",
   },
-  header: {
-    color: "#14b8c4",
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  title: {
-    color: "#fff",
-    fontSize: 23,
-    fontWeight: "bold",
-    marginTop: 10,
-  },
-  content: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "flex-start",
-    paddingTop: 20,
-  },
-  icon: {
-    width: 60,
-    height: 60,
-    tintColor: "#14b8c4",
-    marginVertical: 60,
-  },
-  description: {
+  sectionTitle: {
     color: "#fff",
     fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 30,
     textAlign: "center",
-    marginBottom: 8,
   },
-  status: {
-    color: "#14b8c4",
-    fontSize: 20,
-    marginBottom: 4,
-  },
-  deviceInfo: {
-    color: "#fff",
-    fontSize: 20,
-    marginBottom: 20,
-  },
-  button: {
-    backgroundColor: "#14b8c4",
-    width: 150, 
-    paddingVertical: 20, 
-    borderRadius: 18, 
-    marginHorizontal: 4,
-    alignItems: "center",
-    marginVertical: 8,
-  },
-  buttonText: {
+  sectionDesc: {
     color: "#ffffffff",
+    fontSize: 20,
+  },
+  deviceStatusRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 18,
+    marginBottom: 3,
+    justifyContent: "space-between",
+  },
+  deviceStatusText: {
     fontSize: 16,
     fontWeight: "bold",
+    color: "#fff",
+  },
+  switchWrapper: {
+    marginRight: 3,
+  },
+  divider: {
+    height: 1,
+    backgroundColor: "#242a4f",
+    marginVertical: 15,
+  },
+  pairedTitle: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 17,
+    marginBottom: 7,
+  },
+  deviceBox: {
+    backgroundColor: "#d4dbe6",
+    borderRadius: 14,
+    padding: 10,
+    marginBottom: 15,
+  },
+  deviceRow: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  stickIcon: {
+    width: 24,
+    height: 24,
+    marginRight: 8,
+  },
+  deviceNameConnected: {
+    fontWeight: "bold",
+    fontSize: 16,
+    color: "#111d35",
+    marginRight: 7,
+  },
+  deviceStatusConnected: {
+    color: "#29e664",
+    fontWeight: "500",
+    fontSize: 16,
+    marginLeft: 4,
+  },
+  deviceLastSync: {
+    color: "#000000ff",
+    marginLeft: 34,
+    fontSize: 14,
+    marginTop: 2,
+  },
+  deviceNameNotConnected: {
+    fontWeight: "bold",
+    fontSize: 16,
+    color: "#111d35",
+    marginRight: 9,
+  },
+  deviceStatusNotConnected: {
+    color: "#000000ff",
+    fontSize: 16,
+    fontWeight: "600",
   },
 });
+
+
+
 
